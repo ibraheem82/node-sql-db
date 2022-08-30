0324ed7642dfa42d7bb675a26feb9bc3eae0f4ee
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-30 17:44:12 +0100

    datas table created.

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ app.get('/createdb', (req, res) => {
     });
 });
 
+//* Creating table
+app.get('/createdatastable', (req, res) => {
+    let sql = 'CREATE TABLE datas(id int AUTO_INCREMENT, tltle VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Datas table created........');
+    });
+
+})
+
 app.listen('8000', () => {
     console.log(`Server started on port 8000`);
 });
