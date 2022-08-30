52bbc7495a3efcdc4c1fc7d5732e9517b402a070
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-30 17:59:50 +0100

    data 1 added.

--- app.js
+++ app.js
@@ -34,14 +34,26 @@ app.get('/createdb', (req, res) => {
 
 //* Creating table
 app.get('/createdatastable', (req, res) => {
-    let sql = 'CREATE TABLE datas(id int AUTO_INCREMENT, tltle VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    let sql = 'CREATE TABLE datas(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
     db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
         res.send('Datas table created........');
     });
-
 })
+
+//* Insert data 1
+app.get('/adddata1', (req, res) => {
+    let post = {title: "name", body: "My name is ibraheem omikunle"};
+    // the questions mark (?) is basically like a placeholder that we are putting beside the sql and the post is that post data
+    let sql  = 'INSERT INTO datas SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Data 1 added......');
+
+    });
+});
 
 app.listen('8000', () => {
     console.log(`Server started on port 8000`);
